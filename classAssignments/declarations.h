e620c6099683f132df233b105d08a2641967c6b6
subprimetime <subprimetime@gmail.com>
2015-03-29 09:17:24 -0700

    Updating project structure

---
+++ classAssignments/declarations.h
@@ -0,0 +1,12 @@
+// Place your funtion declarations here
+
+
+#ifndef DECLARATIONS_H
+#define DECLARATIONS_H
+
+int functionCreateStruct();
+int UpdateSalary(struct person *p);
+int PrintStruct(struct person p);
+
+
+#endif
--- classAssignments/main.cpp
+++ classAssignments/main.cpp
@@ -1,12 +1,13 @@
- // classAssignments.cpp : Defines the entry point for the console application.
+ // main.cpp : Includes main function
 
 #include"stdafx.h"
 #include <iostream>
 #include <conio.h>
 #include "gtest/gtest.h"
 #include <Windows.h>
+#include "declarations.h"
 
-
+using namespace std;
 
 int main(int argc, char** argv)
 {
