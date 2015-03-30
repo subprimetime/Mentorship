d372117ec2c2a1a60874d94b88c91fa609f4de21
subprimetime <subprimetime@gmail.com>
2015-03-30 00:04:07 -0700

    Project cpp

---
+++ classAssignments/structAssignment.cpp
@@ -0,0 +1,33 @@
+#include"stdafx.h"
+#include <iostream>
+#include "declarations.h"
+
+using namespace std;
+
+namespace marcus
+{
+
+}
+
+namespace negin
+
+{
+
+
+
+}
+
+namespace erika
+{
+
+}
+
+namespace elmira
+{
+
+}
+
+namespace giang
+{
+
+}
