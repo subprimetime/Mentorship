f5bf73f15d8f9b69349529c4c0720af094814365
erikaroy <erika.roy@divx.com>
2015-04-07 13:42:13 -0700

    adding my assignment (not complete)

--- classAssignments/declarations.h
+++ classAssignments/declarations.h
@@ -5,9 +5,13 @@
 #define DECLARATIONS_H
 
 void functionCreateStruct();
+void er_functionsCreateStruct();
 int UpdateSalary(struct person *p);
+int UpdateSal(struct person *p);
 int PrintStruct(struct person p); //  FYI - This function only accepts one element of an array.
+int er_PrintStruct(struct person p);
 char mar_PrintStruct(struct person *p); // FYI this accepts a full array to be passed into it.
+char er_PrintStruct(struct person *p);
 
 struct person
 {
--- classAssignments/struct_erika.cpp
+++ classAssignments/struct_erika.cpp
@@ -1,5 +1,59 @@
 #include"stdafx.h"
 #include <iostream>
 #include "declarations.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
+
+void er_functionCreateStruct()
+
+{
+	struct person p[10]; //array of 10 people
+	char* names[10] = { "Homer", "Marge", "Bart", "Lisa", "Maggie", "Ned", "Itchy", "Scratchy", "Krusty", "Apu" };
+	for (int i = 0; i < 10; i++)
+
+	{
+		strcpy_s(p[i].name, names[i]);
+		p[i].salary = 10;
+	}
+	UpdateSal(p); // call to function to update salary
+}
+
+int UpdateSal(struct person *p) // update salary of p
+
+{
+	float newSalary = 125;
+	for (int i = 0; i < 10; i++)
+	{
+		p[i].salary = 125;
+		memcpy(&p[i].salary, &newSalary, sizeof(p[i].salary));
+		er_PrintStruct(p[i]); //calls PrintStruct passing only one elements.
+	}
+			{
+				er_PrintStruct(p);
+			}
+				{
+					return 0;
+				}
+}
+
+int er_PrintStruct(struct person p) // function accepts only one array element at a time.
+{
+	{
+		cout << "The Salary for " << p.name << "  is:" << p.salary << "\n";
+	}
+				{
+					return 0;
+				}
+}
+
+char er_PrintStruct(struct person *p) // Function to print when whole p.name array is passed.
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "The Salary for " << p[i].name << "  is:" << p[i].salary << "\n";
+	}
+	return 0;
+}
