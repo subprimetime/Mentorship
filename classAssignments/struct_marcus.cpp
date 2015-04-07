5d9c084497437958e9a1a563cdf25fc1c96781f9
subprimetime <subprimetime@gmail.com>
2015-04-07 13:44:30 -0700

    Added Doxygen comments

--- classAssignments/struct_marcus.cpp
+++ classAssignments/struct_marcus.cpp
@@ -1,20 +1,19 @@
 #include"stdafx.h"
-
-using namespace std;
-
-
 #include <iostream>
 #include <string.h>
 #include "declarations.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+using namespace std;
 
+/*!This function creates an struct person object (p[10]) and adds 10 names to it.
 
+*/
 	void functionCreateStruct()
 	{
 
-		struct person p[10]; // array of 10 people objects
+		struct person p[10]; // array of 10 names
 
 		char* people[10] = { "amit", "marcus", "steve", "giang", "erika", "negin", "fran", "elmira", "cristina", "bob" };
 
@@ -27,6 +26,11 @@ using namespace std;
 		UpdateSalary(p); // Call to function that updates salary of object p
 	}
 
+	/*!This function addes a newSalary value to p[i].salary
+	@param[in] struct person *p
+
+	@return 0 if there is an error.
+	*/
 	int UpdateSalary(struct person *p) // Updates Salary to person p
 
 	{
@@ -48,7 +52,11 @@ using namespace std;
 				}
 	
 	}
+	/*!This function takes a base elenent of p and prints a result.
+	@param[in] struct person p
 
+	@return 0 if there is an error.
+	*/
 	int PrintStruct(struct person p) // function accepts only one array element at a time.
 	{
 		{
@@ -60,7 +68,11 @@ using namespace std;
 				return 0;
 				}
 	}
+	/*!This function takes an array of *p and prints a result.
+	@param[in] struct person *p
 
+	@return 0 if there is an error.
+	*/
 	char mar_PrintStruct(struct person *p) // Function to print when whole p.name array is passed.
 	{
 
@@ -69,8 +81,9 @@ using namespace std;
 			
 			cout << "The Salary for " << p[i].name << "  is:" << p[i].salary << "\n";
 		}
-		
-		return 0;
+				{
+					return 0;
+				}
 	}
 	
 	
